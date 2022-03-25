235f649b6965701ef4f0bb06261b53d20db850f8
Martin <mwgoff24@students.bmchs.org>
2022-03-25 09:49:33 -0500

    worked on a few things

--- project3.py
+++ project3.py
@@ -4,7 +4,7 @@ import random
 # introduction to game
 print("Welcome to the Oregon Trail! \n"
 "You are in Independence, Missouri, and you have 500 pounds of food and 5 health. \n"
-"It is 03/01. You must travel 2000 miles to Oregon by 12/31. \n")
+"It is March 1. You must travel 2000 miles to Oregon by December 31. \n")
 
 # asks for name
 name = input("What is your name? ")
@@ -20,9 +20,10 @@ current_month = 3
 
 # function defs
 def add_day(days):
-    global current_day, current_month
+    global current_day, current_month, food_pounds
     months_31_days = [3, 5, 7, 8, 10, 12]
     current_day += days
+    food_pounds -= days * 5
     if current_day > 30 and current_month / 2 != str:
         current_day -= 30
         current_month += 1
@@ -32,20 +33,31 @@ def add_day(days):
             current_month += 1
 
 def travel():
-    global miles_to_go, current_day
+    global miles_to_go, current_day, food_pounds, player_health
     miles_to_go -= random.randint(30, 61)
     days_travelled = random.randint(3, 8)
     add_day(days_travelled)
     if miles_to_go < 0:
         miles_to_go = 0
-    print(f"You have {miles_to_go} miles left. Today is {MONTHS[current_month]} {current_day}. \n")
+    elif food_pounds < 0:
+        food_pounds = 0
+    print(f"You have {miles_to_go} miles left. Today is {MONTHS[current_month]} {current_day}. You have {food_pounds} pounds of food left. Your health is at {player_health}. \n")
 
 def hunt():
     global food_pounds
     days_hunted = random.randint(2, 6)
     add_day(days_hunted)
-    food += 100
-    
+    food_pounds += 100
+    print("Nice! You now have 100 more pounds of food. It is now {MONTHS[current_month]} {current_day}. \n")
+
+def rest():
+    global player_health
+    days_rested = random.randint(2, 6)
+    add_day(days_rested)
+    player_health += 1
+    if player_health >= 5:
+        print(f"What are you resting for, you lazy person? It is now {MONTHS[current_month]} {current_day}! You're wasting time! \n")
+        player_health -=1
 
 def status():
     print(f"Your health: {player_health}. \n"
@@ -74,12 +86,19 @@ while True:
     elif current_month > 12:
         print("Oh no! You didn't make it to Oregon on time!")
         break
+    elif food_pounds == 0:
+        print("Oops! You ran out of food!")
+        break
     else:
         pass
 
     action = input(f"What would you like to do, {name}? ")
     if action == 'travel':
         travel()
+    elif action == 'hunt':
+        hunt()
+    elif action == 'rest':
+        rest()
     elif action == 'status':
         status()
     elif action == 'quit':
@@ -89,8 +108,3 @@ while True:
         help()
     else:
         print("That's not a function. In this game, you can either travel, hunt, rest, show your status, ask for help, or quit out of the game. \n")
-    
-    
-    
-    
-    
